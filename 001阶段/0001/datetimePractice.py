62819299f2635d5fd8bf526c89629544c182b2b7
zzuwhj@126.com <wanghongjun855@pingan.com.cn>
2018-07-13 09:56:07 +0800

    add

--- 001阶段/0001/datetimePractice.py
+++ 001阶段/0001/datetimePractice.py
@@ -1,7 +1,53 @@
 # coding=utf-8
 # -*-coding:utf-8-*-
 
-# datetime 练习
-import datetime
+# time & datetime 练习
+'''
+%y 两位数的年份表示（00-99）
+%Y 四位数的年份表示（000-9999）
+%m 月份（01-12）
+%d 月内中的一天（0-31）
+%H 24小时制小时数（0-23）
+%I 12小时制小时数（01-12）
+%M 分钟数（00=59）
+%S 秒（00-59）
+%a 本地简化星期名称
+%A 本地完整星期名称
+%b 本地简化的月份名称
+%B 本地完整的月份名称
+%c 本地相应的日期表示和时间表示
+%j 年内的一天（001-366）
+%p 本地A.M.或P.M.的等价符
+%U 一年中的星期数（00-53）星期天为星期的开始
+%w 星期（0-6），星期天为星期的开始
+%W 一年中的星期数（00-53）星期一为星期的开始
+%x 本地相应的日期表示
+%X 本地相应的时间表示
+%Z 当前时区的名称
+%% %号本身
+'''
 
-print("当前时间: %s".decode('UTF-8').encode('GBK') %datetime.datetime.now())
+'''
+------------------------------------------------------------------------------------
+From                       |To                        |Use                         |
+seconds since the epoch    |struct_time in UTC        |gmtime(([secs])             |
+seconds since the epoch    |struct_time in local time |localtime(([secs])          |
+struct_time in UTC         |seconds since the epoch   |calendar.timegm()           |
+struct_time in local time  |seconds since the epoch   |mktime()                    |
+struct_time                |Formatting time           |strftime([9-item sequence]) |
+------------------------------------------------------------------------------------
+'''
+
+import datetime, time, calendar
+
+now_timestamp = time.time()
+now_time = time.localtime()
+#utc_time = calendar.timegm()
+now_ftime = time.strftime('%Y-%m-%d %H:%M:%S')
+#now_ftime=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+print("当前时间戳:\t%s".decode('UTF-8').encode('GBK') % now_timestamp)
+print("当前时间:\t%s".decode('UTF-8').encode('GBK') % now_time)
+print("格式化时间:\t%s".decode("UTF-8").encode("GBK") % now_ftime)
+
+
